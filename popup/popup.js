acaf6e7c10c318345ba8441d47926b2cfe90235f
Shreedhar Bhat <shridharbhat1998@gmail.com>
2020-07-18 08:34:50 +0530

    Context menu added

--- popup/popup.js
+++ popup/popup.js
@@ -48,3 +48,27 @@ function countWords() {
     }
 
 }
+
+document.body.style.border = "5px solid blue";
+// function copySelection() {
+//     var selectedText = window.getSelection().toString().trim();//     if (selectedText) {
+//         document.execCommand("Copy");
+//     }
+// }// /*
+// Add copySelection() as a listener to mouseup events.
+// */
+// document.addEventListener("mouseup", copySelection);
+function onCreated() {
+    if (browser.runtime.lastError) {
+      console.log(`Error: ${browser.runtime.lastError}`);
+    } else {
+      console.log("Item created successfully");
+    }
+  }browser.menus.create({
+    id: "log-selection",
+    title: "Word-Counter",
+    type: "normal",
+    contexts: ["selection"]
+  }, onCreated);  browser.menus.onClicked.addListener((info, tab) => {
+       alert(info.selectionText.length)
+    })
