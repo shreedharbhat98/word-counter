205976377b2d8081f945f5b0ae8abecd5ced6696
Shreedhar Bhat <shridharbhat1998@gmail.com>
2020-07-18 14:25:58 +0530

    text to speech added

--- popup/popup.js
+++ popup/popup.js
@@ -60,18 +60,32 @@ function listenText() {
     speak();
 }
 
+var voices = []
 
+function populateVoiceList() {
+    voices = synth.getVoices().sort(function (a, b) {
+        const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
+        if ( aname < bname ) return -1;
+        else if ( aname == bname ) return 0;
+        else return +1;
+    });
+    for(i = 0; i < voices.length ; i++) {
+      var option = document.createElement('option');
+      option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
 
+      if(voices[i].default) {
+        option.textContent += ' -- DEFAULT';
+      }
 
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+    }
+  }
 
-
-var pitch =1
-var pitchValue =1
-var rate = 1
-var rateValue = 1
-
-var voices = "English_(America)";
-
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
 
 
 
@@ -88,9 +102,15 @@ function speak(){
     utterThis.onerror = function (event) {
         console.error('SpeechSynthesisUtterance.onerror');
     }
-    utterThis.voice = voices;
-    utterThis.pitch = pitch.value;
-    utterThis.rate = rate.value;
+    var selectedOption = "English_(Great_Britain) "
+  for(i = 0; i < voices.length ; i++) {
+    if(voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
+      break;
+    }
+  }
+    utterThis.pitch = 0.9;
+    utterThis.rate = 0.9;
     synth.speak(utterThis);
   }
 }
